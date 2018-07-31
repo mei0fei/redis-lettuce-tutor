09a1bd3ceae76d9bf0558a344aaa1b1578b8a287
mei0fei <mei0fei@qq.com>
2018-07-31 20:49:43 +0800

    first

---
+++ src/main/java/com/example/lettuce/App.java
@@ -0,0 +1,32 @@
+package com.example.lettuce;
+
+/**
+ * Hello world!
+ *
+ */
+
+import io.lettuce.core.*;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+        RedisClient client = RedisClient.create("redis://localhost");        
+
+        StatefulRedisConnection<String, String> connection = client.connect(); 
+
+        RedisCommands<String, String> commands = connection.sync();   
+        commands.set("foo", "hello");
+
+        String value = commands.get("foo");                                   
+        System.out.println(value);
+
+        connection.close();                                                    
+
+        client.shutdown();                                                     
+        
+    }
+}
